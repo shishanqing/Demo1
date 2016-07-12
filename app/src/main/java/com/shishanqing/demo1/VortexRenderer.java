932834fe65cd3497cb7009a9895e64571815dc69
shanqing.shi <shanqing.shi@sim.com>
2016-07-12 10:26:48 +0800

    pyramid

--- app/src/main/java/com/shishanqing/demo1/VortexRenderer.java
+++ app/src/main/java/com/shishanqing/demo1/VortexRenderer.java
@@ -21,29 +21,94 @@ public class VortexRenderer implements GLSurfaceView.Renderer {
     private float blue = 0f;
 
     private ShortBuffer indexBuffer;
+    //private ShortBuffer indexBufferStatic;
     private FloatBuffer vertexBuffer;
+    //private FloatBuffer vertexBufferStatic;
+    private FloatBuffer colorBuffer;
     private short[] indicesArray = { 0, 1, 2 };
-    private int numberOfVertices = 3;
+    private int numberOfVertices = 0;//3;
 
     private float angle;
+    private float xAngle;
+    private float yAngle;
 
     private void initTriangle() {
-        ByteBuffer vbb = ByteBuffer.allocateDirect(numberOfVertices * 3 * 4);
+        float[] coords = {
+                //coodinates
+                -0.5f, -0.5f, 0.5f,//0
+                0.5f, -0.5f, 0.5f,//1
+                0f, -0.5f, -0.5f,//2
+                0f, 0.5f, 0f//3
+                //-0.5f, -0.5f, 0f,//(x1,y1,z1)
+                //0.5f, -0.5f, 0f,//(x2,y2,z2)
+                //-0.5f, 0.5f, 0f//(x3,y3,z3)
+        };
+
+        numberOfVertices = coords.length;
+
+        float[] colors = {
+                //colors
+                1f,0f,0f,1f,//point0  red
+                0f,1f,0f,1f,//point1  green
+                0f,0f,1f,1f,//point2  blue
+                1f,1f,1f,1f //point3  white
+        };
+
+        short[] indices = new short[]{
+                //indices
+                0,1,3,//rwg
+                0,2,1,//rbg
+                0,3,2,//rwb
+                1,2,3 //gbw
+        };
+
+        //ByteBuffer vbb = ByteBuffer.allocateDirect(numberOfVertices * 3 * 4);
+        ByteBuffer vbb = ByteBuffer.allocateDirect(coords.length * 4);
         vbb.order(ByteOrder.nativeOrder());
         vertexBuffer = vbb.asFloatBuffer();
 
-        ByteBuffer ibb = ByteBuffer.allocateDirect(numberOfVertices * 2);
+        //ByteBuffer ibb = ByteBuffer.allocateDirect(numberOfVertices * 2);
+        ByteBuffer ibb = ByteBuffer.allocateDirect(indices.length * 2);
         ibb.order(ByteOrder.nativeOrder());
         indexBuffer = ibb.asShortBuffer();
 
-        float[] coords = { -0.5f, -0.5f, 0f, 0.5f, -0.5f, 0f, 0f, 0.5f, 0f };
+        //ByteBuffer cbb = ByteBuffer.allocateDirect(4 * numberOfVertices * 4);
+        ByteBuffer cbb = ByteBuffer.allocateDirect(colors.length * 4);
+        cbb.order(ByteOrder.nativeOrder());
+        colorBuffer = cbb.asFloatBuffer();
+
+
 
         vertexBuffer.put(coords);
-        indexBuffer.put(indicesArray);
+        //indexBuffer.put(indicesArray);
+        indexBuffer.put(indices);
+        colorBuffer.put(colors);
 
         vertexBuffer.position(0);
         indexBuffer.position(0);
+        colorBuffer.position(0);
     }
+
+    /*private void initStaticTriangle() {
+        ByteBuffer vbb = ByteBuffer.allocateDirect(numberOfVertices * 3 * 4);
+        vbb.order(ByteOrder.nativeOrder());
+        vertexBufferStatic = vbb.asFloatBuffer();
+
+        ByteBuffer ibb = ByteBuffer.allocateDirect(numberOfVertices * 2);
+        ibb.order(ByteOrder.nativeOrder());
+        indexBufferStatic = ibb.asShortBuffer();
+
+        float[] coords = { -0.8f, -0.8f, 0f,
+                0.8f, -0.8f, 0f,
+                0f, 0.8f, 0f
+        };
+
+        vertexBufferStatic.put(coords);
+        indexBufferStatic.put(indicesArray);
+
+        vertexBufferStatic.position(0);
+        indexBufferStatic.position(0);
+    }*/
 
     public void setColor(float red, float green, float blue) {
         this.red = red;
@@ -55,10 +120,35 @@ public class VortexRenderer implements GLSurfaceView.Renderer {
         this.angle = angle;
     }
 
+    public void setxAngle(float angle){
+        xAngle = angle;
+    }
+
+    public void setyAngle(float angle){
+        yAngle = angle;
+    }
+
+    public float getxAngle(){
+        return xAngle;
+    }
+
+    public float getyAngle(){
+        return yAngle;
+    }
+
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+        //preparation
+        //enable the differentiation of which side may be visable
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glFrontFace(GL10.GL_CCW);
+        gl.glCullFace(GL10.GL_BACK);
+
         gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+
         initTriangle();
+        //initStaticTriangle();
     }
 
     @Override
@@ -68,11 +158,24 @@ public class VortexRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onDrawFrame(GL10 gl) {
-        gl.glClearColor(red, green, blue, 1.0f);
+        //gl.glClearColor(red, green, blue, 1.0f);
+        gl.glClearColor(0f, 0f, 0f, 1.0f);
+        gl.glLoadIdentity();
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-        gl.glRotatef(angle, 0f, 1f, 0f);
-        gl.glColor4f(0.5f, 0f, 0f, 0.5f);
+
+        gl.glRotatef(xAngle,1f,0f,0f);
+        gl.glRotatef(yAngle,0f,1f,0f);
+        //gl.glRotatef(angle, 0f, 1f, 0f);
+        //gl.glTranslatef(-1.5f,0.0f,6.0f);
+        //gl.glColor4f(0f, 0.5f, 0f, 0.5f);
+        //gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBufferStatic);
+        //gl.glDrawElements(GL10.GL_TRIANGLES, numberOfVertices, GL10.GL_UNSIGNED_SHORT, indexBufferStatic);
+
+        //gl.glRotatef(angle, 0f, 1f, 0f);
+        //gl.glTranslatef(-1.5f,0.0f,6.0f);
+        //gl.glColor4f(0.5f, 0f, 0f, 0.5f);
         gl.glVertexPointer(3, GL10.GL_FLOAT, 0, vertexBuffer);
+        gl.glColorPointer(4,GL10.GL_FLOAT, 0, colorBuffer);
         gl.glDrawElements(GL10.GL_TRIANGLES, numberOfVertices, GL10.GL_UNSIGNED_SHORT, indexBuffer);
     }
 }
--- app/src/main/java/com/shishanqing/demo1/VortexView.java
+++ app/src/main/java/com/shishanqing/demo1/VortexView.java
@@ -10,6 +10,9 @@ import android.view.MotionEvent;
 public class VortexView extends GLSurfaceView {
     private static final String LOG_TAG = VortexView.class.getSimpleName();
     private VortexRenderer renderer;
+    private float x = 0;
+    private float y = 0;
+
 
     public VortexView(Context context) {
         super(context);
@@ -19,13 +22,28 @@ public class VortexView extends GLSurfaceView {
 
     @Override
     public boolean onTouchEvent(final MotionEvent event) {
-        queueEvent(new Runnable() {
-            @Override
-            public void run() {
-                renderer.setColor(event.getX() / getWidth(), event.getY() / getHeight(), 1.0f);
-                renderer.setAngle(event.getX() / 10);
-            }
-        });
-        return super.onTouchEvent(event);
+        if(event.getAction() == MotionEvent.ACTION_DOWN){
+            x = event.getX();
+            y = event.getY();
+        }
+
+        if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            final float xdiff = x - event.getX();
+            final float ydiff = y - event.getY();
+
+            queueEvent(new Runnable() {
+                @Override
+                public void run() {
+                    //renderer.setColor(event.getX() / getWidth(), event.getY() / getHeight(), 1.0f);
+                    //renderer.setAngle(event.getX());
+                    renderer.setxAngle(renderer.getxAngle() + ydiff);
+                    renderer.setyAngle(renderer.getyAngle() + xdiff);
+                }
+            });
+            x = event.getX();
+            y = event.getY();
+        }
+        //return super.onTouchEvent(event);
+        return true;
     }
 }
